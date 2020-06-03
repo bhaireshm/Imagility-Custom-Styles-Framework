34740c52177b29169e73d89cbfb5764658a30c2a
Bhairesh <Bhairesh@arithaconsulting.com>
2020-06-03 10:19:27 +0530

    Js, Css Compiler Update.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,67 @@
 const path = require('path');
 
-module.exports = [
-    {
-        name: 'Index file',
-        entry: './src/js/index.js',
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, 'dist/js/'),
-        },
-    },
-    {
-        name: "bootstrap",
-        entry: {
-            // main: './src/js/index.js',
-            bootstrap: './node_modules/bootstrap/dist/js/bootstrap.js',
-        }, output: {
-            filename: 'bootstrap.js',
-            path: path.resolve(__dirname, 'dist/js/')
-        },
-    },
-    {
-        name: "date",
-        entry: {
-            datetimepicker: './node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js'
-        }, output: {
-            filename: 'datetimepicker.js',
-            path: path.resolve(__dirname, 'dist/js/')
-        },
-    }
-];
+// module.exports = [
+//     {
+//         name: 'index',
+//         entry: './src/js/index.js',
+//         output: {
+//             filename: 'index.js',
+//             path: path.resolve(__dirname, 'dist/js/'),
+//         },
+//     },
+//     {
+//         name: "bootstrap",
+//         entry: {
+//             // main: './src/js/index.js',
+//             bootstrap: './node_modules/bootstrap/dist/js/bootstrap.js',
+//         }, output: {
+//             filename: 'bootstrap.js',
+//             path: path.resolve(__dirname, 'dist/js/')
+//         },
+//     },
+//     {
+//         name: "date",
+//         entry: {
+//             datetimepicker: './node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js'
+//         }, output: {
+//             filename: 'datetimepicker.js',
+//             path: path.resolve(__dirname, 'dist/js/')
+//         },
+//     }
+// ];
+
+
+module.exports = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(
+                {
+                    entry: './src/js/index.js',
+                    output: {
+                        filename: 'index.js',
+                        path: path.resolve(__dirname, 'dist/js/')
+                    },
+                    mode: 'production',
+                },
+                {
+                    entry: {
+                        bootstrap: './node_modules/bootstrap/dist/js/bootstrap.js',
+                    }, output: {
+                        filename: 'bootstrap.js',
+                        path: path.resolve(__dirname, 'dist/js/')
+                    },
+                    mode: 'production',
+                },
+                {
+                    entry: {
+                        datetimepicker: './node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js'
+                    }, output: {
+                        filename: 'datetimepicker.js',
+                        path: path.resolve(__dirname, 'dist/js/')
+                    },
+                    mode: 'production',
+                }
+            );
+        }, 5000);
+    });
+};
