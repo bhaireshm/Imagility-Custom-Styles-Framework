0d4b559c39348a46b212009e40fc618167de15f7
Bhairesh <Bhairesh@arithaconsulting.com>
2020-06-03 10:54:01 +0530

    jQuery@3.5.1, Bootstrap@4.5.0 Bundled.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,10 @@
 const path = require('path');
 
-// module.exports = [
-//     {
-//         name: 'index',
-//         entry: './src/js/index.js',
-//         output: {
-//             filename: 'index.js',
-//             path: path.resolve(__dirname, 'dist/js/'),
-//         },
-//     },
-//     {
-//         name: "bootstrap",
-//         entry: {
-//             // main: './src/js/index.js',
-//             bootstrap: './node_modules/bootstrap/dist/js/bootstrap.js',
-//         }, output: {
-//             filename: 'bootstrap.js',
-//             path: path.resolve(__dirname, 'dist/js/')
-//         },
-//     },
-//     {
-//         name: "date",
-//         entry: {
-//             datetimepicker: './node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js'
-//         }, output: {
-//             filename: 'datetimepicker.js',
-//             path: path.resolve(__dirname, 'dist/js/')
-//         },
-//     }
-// ];
-
 module.exports = {
     name: "main",
     entry: [
         "./src/js/index.js",
-        "./node_modules/bootstrap/dist/js/bootstrap.js",
-        // "./node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.js"
+        "./node_modules/bootstrap/dist/js/bootstrap.js"
     ],
     output: {
         path: path.resolve(__dirname, 'dist/js/'),
