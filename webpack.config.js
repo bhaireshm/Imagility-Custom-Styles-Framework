e3aef8dbd7453d0c628132bf5a51b465a3ac5c7f
Bhairesh <Bhairesh@arithaconsulting.com>
2020-06-03 10:49:54 +0530

    Bootstrap & Index file merged, Performance added.

--- webpack.config.js
+++ webpack.config.js
@@ -30,38 +30,20 @@ const path = require('path');
 //     }
 // ];
 
+module.exports = {
+    name: "main",
+    entry: [
+        "./src/js/index.js",
+        "./node_modules/bootstrap/dist/js/bootstrap.js",
+        // "./node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.js"
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist/js/'),
+        filename: "imagility.js"
+    },
+    mode: 'production',
+    performance: {
+        hints: false
+    },
+}
 
-module.exports = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(
-                {
-                    entry: './src/js/index.js',
-                    output: {
-                        filename: 'index.js',
-                        path: path.resolve(__dirname, 'dist/js/')
-                    },
-                    mode: 'production',
-                },
-                {
-                    entry: {
-                        bootstrap: './node_modules/bootstrap/dist/js/bootstrap.js',
-                    }, output: {
-                        filename: 'bootstrap.js',
-                        path: path.resolve(__dirname, 'dist/js/')
-                    },
-                    mode: 'production',
-                },
-                {
-                    entry: {
-                        datetimepicker: './node_modules/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js'
-                    }, output: {
-                        filename: 'datetimepicker.js',
-                        path: path.resolve(__dirname, 'dist/js/')
-                    },
-                    mode: 'production',
-                }
-            );
-        }, 5000);
-    });
-};
